6aa34d402fef25d4617e7a2a22871a4df5f0c971
razvancir96 <>
2020-01-12 23:57:17 +0200

    Updates

---
+++ precache-manifest.68571803810cc9a635124116b70679a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7493a15db85a414756e2cee2024788",
+    "url": "/sirluggia-news/index.html"
+  },
+  {
+    "revision": "0a7d67a4169aa3ba0232",
+    "url": "/sirluggia-news/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "ef9b59fcf80e9115dd05",
+    "url": "/sirluggia-news/static/css/main.cb8c4fde.chunk.css"
+  },
+  {
+    "revision": "0a7d67a4169aa3ba0232",
+    "url": "/sirluggia-news/static/js/2.8dca17a7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/sirluggia-news/static/js/2.8dca17a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef9b59fcf80e9115dd05",
+    "url": "/sirluggia-news/static/js/main.6de467e6.chunk.js"
+  },
+  {
+    "revision": "0d81e80ca3a3f663c9d9",
+    "url": "/sirluggia-news/static/js/runtime-main.8eb436df.js"
+  }
+]);
